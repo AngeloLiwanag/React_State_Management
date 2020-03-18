9d144d97c68441ee9cd83cb64da6480c62f72cbc
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2020-03-17 21:34:14 -0700

    Forms with validations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Wrapper from './components/Wrapper';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Wrapper items={['something', 'sombrero', 'wholesome', 'ransom', 'apple', 'react','javascript']}/>
+      <Form/>
     </div>
   );
 }
